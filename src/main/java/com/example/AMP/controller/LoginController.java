f1ef4514214d38558375f8fe55f1f1a636b8cbf6
halusy <nryan120@gmail.com>
2024-01-22 18:57:21 -0700

    Main View Introduced

--- src/main/java/com/example/AMP/controller/LoginController.java
+++ src/main/java/com/example/AMP/controller/LoginController.java
@@ -1,17 +1,22 @@
 package com.example.AMP.controller;
 
+import com.example.AMP.MainApplication;
 import com.example.AMP.helper.JDBC;
+import java.io.IOException;
 import java.sql.*;
-
 import com.example.AMP.helper.LoginVerification;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import java.net.URL;
 import java.time.ZoneId;
 import java.time.zone.ZoneRulesProvider;
@@ -19,10 +24,6 @@ import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.io.Serializable;
-
-import static com.example.AMP.helper.JDBC.connection;
-
-
 public class LoginController implements Initializable {
 
     @FXML private Label ErrorLabel;
@@ -54,8 +55,7 @@ public class LoginController implements Initializable {
         LocationDataLabel.setText(String.valueOf(ZoneId.systemDefault()));
 
     }
-
-    @FXML void onLoginButtonClick(ActionEvent event) throws SQLException {
+    @FXML void onLoginButtonClick(ActionEvent event) throws SQLException, IOException {
 
         String UserUsername = String.valueOf(UsernameField.getCharacters());
         String UserPassword = String.valueOf(PasswordField.getCharacters());
@@ -63,7 +63,12 @@ public class LoginController implements Initializable {
         if (LoginVerification.loginVerfication(UserUsername,UserPassword)){
 
             System.out.println("Login Successful");
-
+            Parent root = FXMLLoader.load(MainApplication.class.getResource("main-schedule-view.fxml"));
+            Stage stage = (Stage) PasswordField.getScene().getWindow();
+            Scene scene = new Scene(root, 720, 400);
+            stage.setTitle("Appointment Management Program (AMP)");
+            stage.setScene(scene);
+            stage.show();
 
         } else {
 
@@ -71,13 +76,7 @@ public class LoginController implements Initializable {
             ErrorLabel.setVisible(true);
 
         }
-
-
-
-
-
     }
-
 }
 
 
---
+++ src/main/java/com/example/AMP/controller/MainScheduleViewController.java
@@ -0,0 +1,17 @@
+package com.example.AMP.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class MainScheduleViewController {
+
+
+    @FXML private Button LogoutButton;
+
+    @FXML void onLogoutButtonClick(ActionEvent event) {
+
+    }
+
+}
