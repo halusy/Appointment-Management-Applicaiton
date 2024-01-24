c6d356520ae7a849b6127d8c3345c2249b5781ab
halusy <nryan120@gmail.com>
2024-01-23 17:53:13 -0700

    Adjusting Button Names in Main View

--- src/main/java/com/example/AMP/controller/MainScheduleViewController.java
+++ src/main/java/com/example/AMP/controller/MainScheduleViewController.java
@@ -6,16 +6,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainScheduleViewController implements Initializable {
 
     @FXML private Button LogoutButton;
-    @FXML private Button addAppointmentButton;
-    @FXML private Button deleteAppointmentButton;
-    @FXML private Button modifyAppointmentButton;
+    @FXML private Button addButton;
+    @FXML private Button deleteButton;
+    @FXML private Button modifyButton;
     @FXML private Button reportsButton;
     @FXML private RadioButton sortAppointmentsAllRadio;
     @FXML private Label sortAppointmentsByLabel;
@@ -24,10 +23,10 @@ public class MainScheduleViewController implements Initializable {
     @FXML private Label titleLabel;
     @FXML private RadioButton viewCustomersRadio;
 
-    @FXML void onAddAppointmentButtonClick(ActionEvent event) {}
-    @FXML void onDeleteAppointmentButtonClick(ActionEvent event) {}
+    @FXML void onAddButtonClick(ActionEvent event) {}
+    @FXML void onDeleteButtonClick(ActionEvent event) {}
     @FXML void onLogoutButtonClick(ActionEvent event) {}
-    @FXML void onModifyAppointmentButtonClick(ActionEvent event) {}
+    @FXML void onModifyButtonClick(ActionEvent event) {}
     @FXML void onReportsButtonClick(ActionEvent event) {}
     @FXML void sortAppointmentsAllRadioToggle(ActionEvent event) {}
     @FXML void sortAppointmentsMonthRadioToggle(ActionEvent event) {}
