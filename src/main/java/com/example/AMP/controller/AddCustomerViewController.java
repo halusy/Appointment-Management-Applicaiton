f5fe6083f063da26dad38e54b157cfdaeca372c5
halusy <nryan120@gmail.com>
2024-01-23 17:49:18 -0700

    All Views Created

---
+++ src/main/java/com/example/AMP/controller/AddCustomerViewController.java
@@ -0,0 +1,4 @@
+package com.example.AMP.controller;
+
+public class AddCustomerViewController {
+}
--- src/main/java/com/example/AMP/controller/MainScheduleViewController.java
+++ src/main/java/com/example/AMP/controller/MainScheduleViewController.java
@@ -2,13 +2,43 @@ package com.example.AMP.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainScheduleViewController {
+public class MainScheduleViewController implements Initializable {
 
     @FXML private Button LogoutButton;
-    @FXML void onLogoutButtonClick(ActionEvent event) {
+    @FXML private Button addAppointmentButton;
+    @FXML private Button deleteAppointmentButton;
+    @FXML private Button modifyAppointmentButton;
+    @FXML private Button reportsButton;
+    @FXML private RadioButton sortAppointmentsAllRadio;
+    @FXML private Label sortAppointmentsByLabel;
+    @FXML private RadioButton sortAppointmentsMonthRadio;
+    @FXML private RadioButton sortAppointmentsWeekRadio;
+    @FXML private Label titleLabel;
+    @FXML private RadioButton viewCustomersRadio;
+
+    @FXML void onAddAppointmentButtonClick(ActionEvent event) {}
+    @FXML void onDeleteAppointmentButtonClick(ActionEvent event) {}
+    @FXML void onLogoutButtonClick(ActionEvent event) {}
+    @FXML void onModifyAppointmentButtonClick(ActionEvent event) {}
+    @FXML void onReportsButtonClick(ActionEvent event) {}
+    @FXML void sortAppointmentsAllRadioToggle(ActionEvent event) {}
+    @FXML void sortAppointmentsMonthRadioToggle(ActionEvent event) {}
+    @FXML void sortAppointmentsWeekRadioToggle (ActionEvent event) {}
+    @FXML void viewCustomersRadioToggle(ActionEvent event) {}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+
 
     }
+
 }
