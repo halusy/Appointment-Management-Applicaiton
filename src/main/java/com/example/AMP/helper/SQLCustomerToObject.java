50992448ba7a40a32951ac1675dd0935a97156e3
halusy <nryan120@gmail.com>
2024-02-01 18:19:30 -0700

    Created SQLCustomerToObject Method.... To Be filled in

--- src/main/java/com/example/AMP/helper/SQLCustomerToObject.java
+++ src/main/java/com/example/AMP/helper/SQLCustomerToObject.java
@@ -1,6 +1,17 @@
 package com.example.AMP.helper;
 
+import com.example.AMP.models.Customer;
+
+import java.sql.SQLException;
+
 public class SQLCustomerToObject {
+
+    public static void SQLCustomerToObjectMethod() throws SQLException {
+
+
+
+
+    }
 
 
 }
